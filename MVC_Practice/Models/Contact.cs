90994f268232d046ef659da5a8c3b766f263caf7
abehringer <abehringer@my.devry.edu>
2019-07-29 13:40:35 -0500

    Added the contact model to the solution / Desktop Git setup

---
+++ MVC_Practice/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Contact
+{
+
+    public int ID { get; set; }
+    public Guid UserID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhonePrimary { get; set; }
+    public string PhoneSecondary { get; set; }
+    public DateTime Birthday { get; set; }
+    public string StreetAddress1 { get; set; }
+    public string StreedAddress2 { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Zip { get; set; }
+
+    public Contact()
+    {
+
+    }
+
+
+
+}
+
